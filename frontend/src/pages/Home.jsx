0771a47ca5639a26ae2c7eac9c62aca8c0a31287
Bhavesh Semwal <bhaveshsemwal@gmail.com>
2025-03-17 17:39:00 +0530

    Your commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
-
-function home() {
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { handleSuccess } from "../utils";
+import { ToastContainer } from "react-toastify";
+function Home() {
+  const [loggedInUser, setLoggedInUser] = useState("");
+  const navigate = useNavigate();
+  useEffect(() => {
+    setLoggedInUser(localStorage.getItem("loggedInUser") || "Guest");
+  }, []);
+  const handleLogout = (e) => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("loggedInUser");
+    handleSuccess("user logged out");
+    setTimeout(() => {
+      navigate("/login");
+    }, 1000);
+  };
   return (
-    <div>home</div>
-  )
+    <div>
+      <h1>{loggedInUser}</h1>
+      <button onClick={handleLogout}>Logout</button>
+      <ToastContainer />
+    </div>
+  );
 }
 
-export default home
+export default Home;
